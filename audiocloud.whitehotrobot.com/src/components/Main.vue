f7930501c3ad9fb7a64639c93eee1dbf1cc458f0
Scott <whitehotrobot@gmail.com>
2021-01-07 14:21:39 -0800

    minor change

--- audiocloud.whitehotrobot.com/src/components/Main.vue
+++ audiocloud.whitehotrobot.com/src/components/Main.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="main" :class="{'footerPadding': state.mode=='track'}">
-    {{state.exact}}
     <div v-if="state.search.string==''">
       <div v-if="state.mode=='track'" class="trackDiv" :class="{'highTop':state.showControls}">
         <div v-if="state.tracks.length" class="flex">
