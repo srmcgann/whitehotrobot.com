4edd94ec92d2b4894a5848a7f6c2e5ffae36f1bf
cantelope <s.r.mcgann@hotmail.com>
2021-02-06 05:19:18 -0800

    fix

--- whitehotrobot.com/src/components/Footer.vue
+++ whitehotrobot.com/src/components/Footer.vue
@@ -34,7 +34,7 @@ export default {
       window.open('https://www.instagram.com/redwoodford/?hl=en', '_blank')
     },
     launchEmail () {
-      location.href = 'mailto: whitehotrobot@gmail.com?subject=I%20come%20from%20the%20Internet!&body=I%20have%20a%20comment%20or%20concern%20about%20the%20website(whitehotrobot.com)...'
+      location.href = 'mailto:whitehotrobot@gmail.com?subject=I%20come%20from%20the%20Internet!&body=I%20have%20a%20comment%20or%20concern%20about%20the%20website(whitehotrobot.com)...'
     },
     launchWeb () {
       window.open('http://audiocloud.whitehotrobot.com/', '_blank')
