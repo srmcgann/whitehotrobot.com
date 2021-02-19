19dc6de51ed42a9fe342554474a7b50837a08055
cantelope <s.r.mcgann@hotmail.com>
2021-02-19 10:56:20 -0800

    sync

--- code.whitehotrobot.com/public/db.php
+++ code.whitehotrobot.com/public/db.php
@@ -3,21 +3,21 @@
   $db_pass="";
   $db_host="localhost";
   $db="videodemos";
-  $maxResultsPerPage = 2;
+  $maxResultsPerPage = 4;
 
-  $local = false;
-  if($local){
-    $baseURL='local.code.whitehotrobot.com';
-    $demoSandbox='local.demo.whitehotrobot.com';
+	$local = false;
+	if($local){
+		$baseURL='local.code.whitehotrobot.com';
+		$demoSandbox='local.demo.whitehotrobot.com';
     $baseAssetsURL = 'http://local.assets.whitehotrobot.com';
   }else{
-    $baseURL='words.whitehotrobot.com';
-    $demoSandbox='demo.whitehotrobot.com';
+		$baseURL='words.whitehotrobot.com';
+		$demoSandbox='demo.whitehotrobot.com';
     $baseAssetsURL = 'https://assets.whitehotrobot.com';
-  }
+	}
 
-  $baseFullURL= ($local ? 'http://' : 'https://') . $baseURL;
-  $link = mysqli_connect($db_host, $db_user, $db_pass, $db);
+	$baseFullURL= ($local ? 'http://' : 'https://') . $baseURL;
+	$link = mysqli_connect($db_host, $db_user, $db_pass, $db);
 ?>
 
 
