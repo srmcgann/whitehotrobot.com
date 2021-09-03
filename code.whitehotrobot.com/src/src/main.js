3f3fa56b6d6e5e90345f23b51d2e03bd7f1d5f02
cantelope <s.r.mcgann@hotmail.com>
2021-09-03 12:04:37 -0700

    sync

--- code.whitehotrobot.com/src/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import linkify from 'vue-linkify'
- 
-import App from './App.vue'
-const myApp = createApp(App)
-
-myApp.directive('linkified', linkify)
-myApp.mount('#app')
